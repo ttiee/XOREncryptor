38d444187c0d70c04b30d431a0acf0d4fd3c78e1
ttiee <469784630@qq.com>
2024-01-21 21:00:03 +0800

    ⚙️ Refact: Refactor the function argument 🔧 Fix some bugs

--- XOREncryptor.py
+++ XOREncryptor.py
@@ -2,7 +2,7 @@
 读取命令行参数，对二进制文件进行加密，解密
 使用异或运算，加密解密使用同一函数
 
-usage: python XOREncryptor.py [-h] [-k KEY] [-d] [-o OUTPUT] path
+usage: python XOREncryptor.py [-h] [-k KEY] [-d] [-o OUTPUT] [-f] path
 
 author: ttiee
 time: 2024/1/18
@@ -23,17 +23,26 @@ from tqdm.rich import tqdm
 warnings.filterwarnings('ignore', category=TqdmExperimentalWarning)
 
 
-def encrypt_file(file_name, key, output_file, force=False):
+def encrypt_file(args):
     """
     加密文件
-    :param file_name: 文件名
-    :param key: 密钥
-    :param output_file: 输出文件名
+    :param args: 命令行参数
     :return:
     """
+    file_name = args.path
+    key = args.key
+    output_file = args.output
+    force = args.force
+
     if os.path.exists(output_file) and not force:
-        print(f'[red]Error: [/red][yellow]Output file already exists: [/yellow][cyan]{output_file}[/cyan]')
+        print(f'[cyan]Output file already exists: [/cyan][yellow]{os.path.abspath(output_file)}[/yellow] [cyan]Use -f to force overwrite[/cyan]')
         return None
+    
+    File_name_Equal_Output_file = os.path.abspath(file_name) == os.path.abspath(output_file)
+    if File_name_Equal_Output_file:
+        os.rename(file_name, file_name + '.tmp')
+        file_name += '.tmp'
+
     with open(file_name, 'rb') as f, open(output_file, 'wb') as f2:
         pbar = tqdm(range(os.path.getsize(file_name)), desc="[green]Encrypting[/green]", unit="B", unit_scale=True, unit_divisor=1024)
         while True:
@@ -46,29 +55,33 @@ def encrypt_file(file_name, key, output_file, force=False):
             f2.write(data)
             pbar.update(len(data))
         pbar.close()
+
+    if File_name_Equal_Output_file:
+        os.remove(file_name)
     
     print(f'[green]Encrypting finished![/green] [cyan]Output file: [/cyan][yellow]{os.path.abspath(output_file)}[/yellow]')
     
 
-def decrypt_file(file_name, key, output_file, force=False):
+def decrypt_file(args):
     """
     解密文件
-    :param file_name: 文件名
-    :param key: 密钥
-    :param output_file: 输出文件名
+    :param args: 命令行参数
     :return:
     """
-    encrypt_file(file_name, key, output_file, force)
+    encrypt_file(args)
 
 
-def encrypt_dir(dir_name, key, output_dir=None, force=False):
+def encrypt_dir(args):
     """
     加密目录下所有文件
-    :param dir_name: 目录名
-    :param key: 密钥
-    :param output_dir: 输出目录
+    :param args: 命令行参数
     :return:
     """
+    dir_name = args.path
+    key = args.key
+    output_dir = args.output
+    force = args.force
+
     if output_dir is None:
         output_dir = os.path.join(os.path.dirname(dir_name), 'encrypted_' + os.path.basename(dir_name))
     if not os.path.exists(output_dir):
@@ -77,20 +90,22 @@ def encrypt_dir(dir_name, key, output_dir=None, force=False):
         file_path = os.path.join(dir_name, file_name)
         output_file = os.path.join(output_dir, file_name)
         if os.path.isfile(file_path):
-            encrypt_file(file_path, key, output_file, force)
+            args.path = file_path
+            args.output = output_file
+            encrypt_file(args)
         elif os.path.isdir(file_path):
-            encrypt_dir(file_path, key, output_file, force)
+            args.path = file_path
+            args.output = output_file
+            encrypt_dir(args)
 
 
-def decrypt_dir(dir_name, key, output_dir=None, force=False):
+def decrypt_dir(args):
     """
     解密目录下所有文件
-    :param dir_name: 目录名
-    :param key: 密钥
-    :param output_dir: 输出目录
+    :param args: 命令行参数
     :return:
     """
-    encrypt_dir(dir_name, key, output_dir, force)
+    encrypt_dir(args)
 
 
 def main():
@@ -107,25 +122,24 @@ def main():
     if args.output is None:
         args.output = os.path.join(os.path.dirname(args.path), 'encrypted_' + os.path.basename(args.path))
 
-    if args.output == args.path:
-        print('[red]Error: [/red][yellow]Output file is the same as input file![/yellow]')
-        sys.exit(1)
-    
     if os.path.isfile(args.path):
         print(f'[cyan]Input file: [/cyan][yellow]{args.path}[/yellow]')
+        os.chdir(os.path.dirname(args.path))
         if args.decrypt:
-            decrypt_file(args.path, args.key, args.output, args.force)
+            decrypt_file(args)
         else:
-            encrypt_file(args.path, args.key, args.output, args.force)
+            encrypt_file(args)
     elif os.path.isdir(args.path):
         print(f'[cyan]Input directory: [/cyan][yellow]{args.path}[/yellow]')
+        os.chdir(os.path.dirname(args.path))
         if args.decrypt:
-            decrypt_dir(args.path, args.key, args.output, args.force)
+            decrypt_dir(args)
         else:
-            encrypt_dir(args.path, args.key, args.output, args.force)
+            encrypt_dir(args)
     else:
         print('[red]Error: [/red][yellow]Path not exists![/yellow]')
         sys.exit(1)
+    sys.exit(0)
 
 
 if __name__ == '__main__':
